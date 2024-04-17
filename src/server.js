1bc4ec43c8265e47ee9d13203017d324512bd5b0
Goran SD <g.cvetic23@gmail.com>
2024-04-16 23:52:37 -0700

    Fix response in server.js to return JSON instead of sending a string

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log("Hello from express");
-  res.status(200).send("Hello World!");
+  res.status(200);
   res.json({
     message: "Hello World!",
   });
 });
 
-module.export = app;
+module.exports = app;
