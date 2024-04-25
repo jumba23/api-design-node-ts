c0229357d717705447e3dbbebdefadf9a4f9a8d5
Goran SD <g.cvetic23@gmail.com>
2024-04-25 15:51:37 -0700

    Add router file with product routes

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.post("/product", () => {});
+router.put("/product/:id", () => {});
+router.delete("/product/:id", () => {});
