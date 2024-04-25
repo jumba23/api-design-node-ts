0aa7a3bf9da3375e3532ec895f3e5fa70fb6dcd6
Goran SD <g.cvetic23@gmail.com>
2024-04-25 15:54:10 -0700

    Add routes for Update model in router.ts

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,21 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * Product routes
+ */
+
 router.get("/product", () => {});
 router.get("/product/:id", () => {});
 router.post("/product", () => {});
 router.put("/product/:id", () => {});
 router.delete("/product/:id", () => {});
+
+/**
+ * Update
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.post("/update", () => {});
+router.put("/update/:id", () => {});
+router.delete("/update/:id", () => {});
