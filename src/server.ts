fb173784870d7247cb4df16f294a360485b1bf3c
Goran SD <g.cvetic23@gmail.com>
2024-04-25 16:22:04 -0700

    Refactor server.ts to use explicit types for request and response objects

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import express from "express";
+import { Request, Response } from "express";
 import router from "./router";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   console.log("Hello from express");
   res.status(200);
-  res.json({
-    message: "Hello World!",
-  });
+  res.json({ message: "Hello World!" });
 });
 
 app.use("/api", router);
