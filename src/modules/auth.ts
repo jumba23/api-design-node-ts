8a2dc10ad2dfe72f6f0d3fef15993557b035876e
Goran SD <g.cvetic23@gmail.com>
2024-04-26 12:19:29 -0700

    Add protect middleware to handle authorization in auth.ts

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -4,3 +4,11 @@ export const createJWT = (user) => {
   const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
   return token;
 };
+
+export const protect = (req, res) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
