82eb8240f04198c4fd03ca9b77c12e09a5ca2428
Goran SD <g.cvetic23@gmail.com>
2024-04-26 12:25:24 -0700

    Update protect middleware in auth.ts to handle token verification and user assignment

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -5,10 +5,26 @@ export const createJWT = (user) => {
   return token;
 };
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
     res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401).json({ message: "not valid token" });
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "not valid token" });
+    return;
   }
 };
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { protect } from "./modules/auth";
 
 const app = express();
 
@@ -17,6 +18,6 @@ app.get("/", (req, res, next) => {
   res.json({ message: "Hello World!" });
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
 
 export default app;
