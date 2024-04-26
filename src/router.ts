eec74a115397e187132000e80325260f0a6b0daa
Goran SD <g.cvetic23@gmail.com>
2024-04-26 14:07:01 -0700

    Add input validation for product name in PUT /product/:id route

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -11,7 +12,13 @@ router.get("/product", (req, res) => {
 });
 router.get("/product/:id", () => {});
 router.post("/product", () => {});
-router.put("/product/:id", () => {});
+router.put("/product/:id", body('name'), (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+});
 router.delete("/product/:id", () => {});
 
 /**
