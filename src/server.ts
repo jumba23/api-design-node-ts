3f225f3cf41f16e89cfaec7673112e04f15ddaaf
Goran SD <g.cvetic23@gmail.com>
2024-04-26 12:00:12 -0700

    Add cors middleware to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
-// import { Router } from "express";
 import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 // global middleware
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
