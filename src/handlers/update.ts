ae0f3b198b510c4aa68b1f41976f016d99f464a9
Goran SD <g.cvetic23@gmail.com>
2024-04-30 11:01:10 -0700

    Refactor deleteUpdate handler to include product updates in update.ts

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -79,4 +79,35 @@ export const updateUpdate = async (req, res) => {
 
   res.json({ data: updatedUpdate });
 };
-export const deleteUpdate = async (req, res) => {};
+
+// Delete an update
+export const deleteUpdate = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      id: req.body.productId,
+    },
+    include: {
+      updates: true,
+    },
+  });
+
+  // Flatten the updates array
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.updates];
+  }, []);
+
+  // Find the update
+  const match = updates.find((update) => update.id === req.params.id);
+
+  if (!match) {
+    return res.status(404).json({ error: "Update not found" });
+  }
+
+  const deleted = await prisma.update.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json({ data: deleted });
+};
