8939e675f59c133ed8853122cdc681ea03f8d28e
Goran SD <g.cvetic23@gmail.com>
2024-04-30 09:58:33 -0700

    Add update handlers for updates

---
+++ src/handlers/update.ts
@@ -0,0 +1,6 @@
+import prisma from "../db";
+
+export const getOneUpdate = async (req, res) => {};
+export const getUpdates = async (req, res) => {};
+export const updateUpdate = async (req, res) => {};
+export const deleteUpdate = async (req, res) => {};
