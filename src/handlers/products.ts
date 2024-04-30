804b8403acd654cd602d247c1f5d4d9432b14733
Goran SD <g.cvetic23@gmail.com>
2024-04-30 09:28:00 -0700

    Add deleteProduct handler to delete a product

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -53,3 +53,13 @@ export const updateProduct = async (req, res) => {
 
   res.json({ data: update });
 };
+
+// Delete a product
+export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id: req.params.id,
+      belongsToId: req.user.id,
+    },
+  });
+};
