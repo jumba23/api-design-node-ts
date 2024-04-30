a4ac25698d03c32540be7cc2d66c8872cabd573f
Goran SD <g.cvetic23@gmail.com>
2024-04-30 10:00:37 -0700

    Update getOneUpdate handler to include response JSON

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -1,6 +1,14 @@
 import prisma from "../db";
 
-export const getOneUpdate = async (req, res) => {};
+export const getOneUpdate = async (req, res) => {
+  const update = await prisma.update.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json({ data: update });
+};
 export const getUpdates = async (req, res) => {};
 export const updateUpdate = async (req, res) => {};
 export const deleteUpdate = async (req, res) => {};
